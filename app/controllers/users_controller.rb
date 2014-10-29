e264d10c9ac83a742a122212dc197f611ad0fb8b
Julian Cheal <julian.cheal@gmail.com>
2014-10-29 00:20:52 -0400

    Added finish_signup to the before_action set_user so it has access to the @ user var.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :finish_signup, :update, :destroy]
 
   # GET /users/:id.:format
   def show
