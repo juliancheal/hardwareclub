d25b7ab45f91b66563d61f577c829eafcc75d37a
Julian Cheal <julian.cheal@gmail.com>
2015-01-10 04:53:34 +0200

    Added to_time convert seconds to hours::minutes::seconds

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -15,4 +15,14 @@ class Episode < ActiveRecord::Base
   def title
     self.name
   end
+  
+  def to_time
+    time = self.seconds
+    hours = time/3600.to_i
+    minutes = (time/60 - hours * 60).to_i
+    
+    seconds = (time - (minutes * 60 + hours * 3600))
+    
+    sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
