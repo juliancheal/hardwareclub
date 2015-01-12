bdf02fca81dc810193659b0ac75707debf121545
Julian Cheal <julian.cheal@gmail.com>
2015-01-12 03:44:50 +0200

    They say video killed the radio star. Oh well. Now with our own video player courtesy of videojs! I had to convert their stylesheet from Less to Sass, but apart from that seems pretty neto.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def videojs(*options, &blk)
+    default_options = {
+      controls: true,
+      setup: "{}",
+      preload: "auto"
+    }
+    options = default_options.merge(options.extract_options!)
+    render partial: 'video/video', locals: { options: options }, &blk
+  end
 end
